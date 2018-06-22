f1266dc3cf87fb4ad8a524f26e649fa445166fba
« <emmajouffroypro@gmail.com>
2018-06-22 09:54:28 +0545

    essayer d'enlever le blanc

--- wp-content/themes/MoveNepal/page-contact.php
+++ wp-content/themes/MoveNepal/page-contact.php
@@ -104,7 +104,7 @@
 				<div class = "row">
 					<div class="form-group">
 						<div class="col-sm-6 col-sm-offset-5">
-							<button type="submit" class="btn btn-default btn-info btn-lg" >Save</button>
+							<button type="submit" class="btn btn-default btn-info btn-lg" >Send</button>
 						</div>
             		</div>
 				</div>
