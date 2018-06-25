4ee124d3be64054805ac80db9bd0fce18e3bf2bd
Noémie Mathio <nmathio@ensc.fr>
2018-06-25 07:33:36 +0200

    nj

--- wp-content/themes/MoveNepal/footer.php
+++ wp-content/themes/MoveNepal/footer.php
@@ -1,36 +1,93 @@
-		
 <div class="container-fluid">
-        <footer>
-            <div class="row">
-                <div class="col-md-3 border-right footer_div">
-                    <div class="container-fluid">
-                        <div class="footer_logo"  style="background-image: url(<?php bloginfo('template_directory')?>/images/logo.png")> </div>
-                    </div>
-                    <div class="footer_police">Budhi Bazar, Lekhnath, Kaski, Pokhara Nepal</div>
-                </div>
-                <div class="col-md-3 border-right footer_div">
-                    <div class="footer_gras"><strong>Move Nepal</strong></div>
-                    <div class="footer_police">We help social projects in Nepal <br>by sending YOU right there.</div>
+    <footer>
 
-                    <div class="footer_gras">Linked association</div>
-                    <div class="footer_gras" href="betterplace.org">
-                        <div class="footer_gras">betterplace.<div class="org">org</div></div>
-                    </div>
-                    <div class="footer_gras">iec</div>
+<?php //Création de la loop d'articles:
+//Verification de la presence de contenu:
+//on utilise le format if/endif pour rester cohérent avec le reste du code wordpress:
+if(have_posts()):
+    while(have_posts()){
+        ?>
 
-
+        <?php
+        the_post();
+        // Get 'valeurs' posts
+        $footer = get_posts(array(
+            'post_type' => 'Footer',
+            'posts_per_page' => -1, //posts illimités
+        ));
+        ?>
 
 
-                </div>
-                <div class="col-md-3 border-right footer_div"> <?php  //on choisit d'abord quel theme
-                    wp_nav_menu(array('theme_location'=>'secondary')); ?></div>
-                <div class="col-md-3 footer_div">
-                    <div class="footer_police"><a href="mailto:info@move-nepal.org"> info@move-nepal.org </a> </div>
-                    <div class="footer_police"><a href="tel:+97761441947">+977 61441947</a></div>
-                </div>
+                <?php
+                if ($footer){ ?>
+
+                    <?php
+                    foreach ( $footer as $post){
+                        setup_postdata($post);
+
+                        ?>
+                        <div class="row container2">
+
+
+                                    <div class="col-md-3 border-right footer_div">
+                                        <div class="footer_logo">
+                                            <img src="<?php echo get_field('logo')['url']; ?>">
+                                        </div>
+                                        <div class="footer_police adress">
+                                            <?php echo get_field('adress')?>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-md-3 border-right footer_div">
+
+                                        <div class="footer_police">
+                                            <strong><?php echo get_field('title_1') ;?></strong>
+                                        </div>
+                                        <div class="footer_police contain">
+                                            <?php echo get_field('container_title_1') ;?>
+                                        </div>
+
+                                        <div class="footer_police">
+                                            <strong><?php echo get_field('title_2') ;?></strong>
+                                        </div>
+                                        <div class="footer_police" >
+                                            <a href="http://betterplace.org"><img src="<?php echo get_field('link_1')['url']; ?>"></a>
+                                        </div>
+                                        <div class="footer_police logo2">
+                                            <a href="http://iec.org"><img src="<?php echo get_field('link_2')['url']; ?>"></a>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="col-md-3 border-right footer_div"> <?php
+                                        wp_nav_menu(array('theme_location'=>'secondary')); ?>
+                                    </div>
+
+                                    <div class="col-md-3 footer_div div_4">
+                                        <div class="footer_police"><a href="mailto:info@move-nepal.org"> <?php echo get_field('email') ;?> </a> </div>
+                                        <div class="footer_police"><a href="tel:+97761441947"><?php echo get_field('phone') ;?></a></div>
+                                        <div class="footer_police" href="betterplace.org"><a href="http://facebook.com">
+                                                <img src="<?php echo get_field('logo_facebook')['url']; ?>"></a>
+                                        </div>
+                                    </div>
+
+
+                        </div>
+
+                        <?php
+                    }
+                }
+                ?>
+                <hr>
             </div>
-        </footer>
+        </div>
+    <?php }
+endif;
+?>
+    </footer>
 </div>
+
 
         <?php //zone d'écriture des scripts JS
 		 wp_footer() ?>
