25a5ba05b330a47dc136ad97dc5f2bd0cd0a491a
Noémie Mathio <nmathio@ensc.fr>
2018-06-26 12:43:49 +0200

    header footer

--- wp-content/themes/MoveNepal/footer.php
+++ wp-content/themes/MoveNepal/footer.php
@@ -1,11 +1,11 @@
 <div class="container-fluid">
     <footer>
 
-<?php //Création de la loop d'articles:
-//Verification de la presence de contenu:
-//on utilise le format if/endif pour rester cohérent avec le reste du code wordpress:
-if(have_posts()):
-    while(have_posts()){
+        <?php //Création de la loop d'articles:
+        //Verification de la presence de contenu:
+        //on utilise le format if/endif pour rester cohérent avec le reste du code wordpress:
+        if(have_posts()):
+        while(have_posts()){
         ?>
 
         <?php
@@ -18,78 +18,76 @@ if(have_posts()):
         ?>
 
 
-                <?php
-                if ($footer){ ?>
+        <?php
+        if ($footer){ ?>
 
-                    <?php
-                    foreach ( $footer as $post){
-                        setup_postdata($post);
-
-                        ?>
-                        <div class="row container2">
-
-
-                                    <div class="col-md-3 col-xs-3 border-right footer_div">
-                                        <div class="footer_logo" >
-                                            <img src="<?php echo get_field('logo')['url']; ?>">
-                                        </div>
-                                        <div class="footer_police adress">
-                                            <?php echo get_field('adress')?>
-                                        </div>
-                                    </div>
+            <?php
+            foreach ( $footer as $post){
+                setup_postdata($post);
+                ?>
+                <div class="row container2">
 
 
-                                    <div class="col-md-3 col-xs-3 border-right footer_div">
-
-                                        <div class="footer_police">
-                                            <strong><?php echo get_field('title_1') ;?></strong>
-                                        </div>
-                                        <div class="footer_police contain">
-                                            <?php echo get_field('container_title_1') ;?>
-                                        </div>
-
-                                        <div class="footer_police">
-                                            <strong><?php echo get_field('title_2') ;?></strong>
-                                        </div>
-                                        <div class="footer_police" >
-                                            <a href="http://betterplace.org"><img src="<?php echo get_field('link_1')['url']; ?>"></a>
-                                        </div>
-                                        <div class="footer_police logo2">
-                                            <a href="http://iec.org"><img src="<?php echo get_field('link_2')['url']; ?>"></a>
-                                        </div>
-
-                                    </div>
-                                    <div class="col-md-3 col-xs-3 border-right footer_div"> <?php
-                                        wp_nav_menu(array('theme_location'=>'secondary')); ?>
-                                    </div>
-
-                                    <div class="col-md-3 col-xs-3 footer_div div_4">
-                                        <div class="footer_police"><a href="mailto:info@move-nepal.org"> <?php echo get_field('email') ;?> </a> </div>
-                                        <div class="footer_police"><a href="tel:+97761441947"><?php echo get_field('phone') ;?></a></div>
-                                        <div class="footer_police" href="betterplace.org"><a href="http://facebook.com">
-                                                <img src="<?php echo get_field('logo_facebook')['url']; ?>"></a>
-                                        </div>
-                                    </div>
+                    <div class="col-md-3 col-lg-3 col-xs-3 border-right footer_div">
+                        <div class="footer_logo" >
+                            <img src="<?php echo get_field('logo')['url']; ?>">
+                        </div>
+                        <div class="footer_police adress">
+                            <?php echo get_field('adress')?>
+                        </div>
+                    </div>
 
 
+                    <div class="col-md-3 col-lg-3 col-xs-3 footer_center border-right footer_div">
+
+                        <div class="footer_police">
+                            <strong><?php echo get_field('title_1') ;?></strong>
+                        </div>
+                        <div class="footer_police contain">
+                            <?php echo get_field('container_title_1') ;?>
                         </div>
 
-                        <?php
-                    }
-                }
-                ?>
-                <hr>
-            </div>
-        </div>
-    <?php }
+                        <div class="footer_police">
+                            <strong><?php echo get_field('title_2') ;?></strong>
+                        </div>
+                        <div class="footer_police" >
+                            <a href="http://betterplace.org"><img src="<?php echo get_field('link_1')['url']; ?>"></a>
+                        </div>
+                        <div class="footer_police logo2">
+                            <a href="http://iec.org"><img src="<?php echo get_field('link_2')['url']; ?>"></a>
+                        </div>
+
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xs-3 div_3 border-right footer_div"> <?php
+                        wp_nav_menu(array('theme_location'=>'secondary')); ?>
+                    </div>
+
+                    <div class="col-md-3 col-lg-3 col-xs-3 footer_div div_4 footer_center">
+                        <div class="footer_police"><a href="mailto:info@move-nepal.org"> <?php echo get_field('email') ;?> </a> </div>
+                        <div class="footer_police"><a href="tel:+97761441947"><?php echo get_field('phone') ;?></a></div>
+                        <div class="footer_police" href="betterplace.org"><a href="http://facebook.com">
+                                <img src="<?php echo get_field('logo_facebook')['url']; ?>"></a>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <?php
+            }
+        }
+        ?>
+        <hr>
+</div>
+</div>
+<?php }
 endif;
 ?>
-    </footer>
+</footer>
 </div>
 
 
-        <?php //zone d'écriture des scripts JS
-		 wp_footer() ?>
-	</body>
+<?php //zone d'écriture des scripts JS
+wp_footer() ?>
+</body>
 </html>
-
--- wp-content/themes/MoveNepal/header.php
+++ wp-content/themes/MoveNepal/header.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>MoveNepal</title>
-	<?php //On dit à worpress que c'est ici que doivent se print les styles:
-	wp_head(); ?>
+    <meta charset="utf-8">
+    <title>MoveNepal</title>
+    <?php //On dit à worpress que c'est ici que doivent se print les styles:
+    wp_head(); ?>
 </head>
 
 <body <?php body_class();?>
@@ -30,16 +30,17 @@
     <div class="container-fluid navb">
 
         <div class="row ligne1 text-center">
-            <div class="col-sm-3">
-                <img src="<?php echo bloginfo('template_directory')?>/images/f.png">
+            <div class="col-sm-3 header_logo">
+                <img src="<?php echo bloginfo('template_directory')?>/images/logo4.png">
             </div>
             <div class="col-sm-5"></div>
             <div class="col-sm-2 experience">
                 <strong> <?php bloginfo('description')?> </strong>
             </div>
-            <div class="col-sm-2">
-                <button type="button" class="btn apply"> <a href=<?php bloginfo('template_directory') ?>"/page-contact.php"?> <strong>APPLY</strong> </a>   <img src="<?php echo bloginfo('template_directory')?>/images/right-arrow.png"></button>
+            <div class="col-sm-2 ">
+                <button type="button" class="btn apply"> <a href=<?php bloginfo('template_directory') ?>"/page-contact.php"?> <strong>APPLY</strong> <img src="<?php echo bloginfo('template_directory')?>/images/right-arrow.png"></a></button>
             </div>
+
         </div>
 
         <div class="row ligne2">
@@ -57,26 +58,26 @@
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
-							<?php  //on choisit d'abord quel theme
-							$arg = (array(
-								'theme_location'=>'primary',
-								'container'=>false,
-								'menu_class'=> 'nav navbar-nav navbar-right',
-								'walker'=> new Walker_Nav_Primary()
-							)
-							);
-							wp_nav_menu($arg);?>
+                            <?php  //on choisit d'abord quel theme
+                            $arg = (array(
+                                'theme_location'=>'primary',
+                                'container'=>false,
+                                'menu_class'=> 'nav navbar-nav navbar-right',
+                                'walker'=> new Walker_Nav_Primary()
+                            )
+                            );
+                            wp_nav_menu($arg);?>
                         </ul>
                     </div><!-- /.navbar-collapse -->
             </nav>
         </div>
 
     </div>
-    
+
     <div class="container">
-                <ol class="breadcrumb">
+        <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="<?php echo get_home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-            <?php 
+            <?php
             $parentID = wp_get_post_parent_id( get_the_ID() );
             while ($parentID){
                 ?>
