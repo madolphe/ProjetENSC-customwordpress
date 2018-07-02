ade363e125d659bd24096a5dad2d2e072d9b0a95
aloizel <aloizel@ensc.fr>
2018-07-02 07:43:31 +0200

    Page contact

--- wp-content/themes/MoveNepal/htmlContact.php
+++ wp-content/themes/MoveNepal/htmlContact.php
@@ -2,24 +2,30 @@
     <div class="card mb-3 p-3" id="form-part-1">
       <div class="card-block">
         <div class="form-group row">
+
           <div class="col-sm-8 col-sm-offset-2">
-            <label class="form-control-label" for="first-name">First Name</label>
-            <input class="form-control" type="text" placeholder="Enter your first name" id="first-name" name="a-first-name">
+            <div class="field">
+              <label class="field-label" for="a-first-name">First Name</label>
+              <input class="field-input" type="text" placeholder="" id="first-name" name="a-first-name">
+            </div>
+          </div>
+
+          <div class="col-sm-8 col-sm-offset-2">
+            <div class="field">
+              <label class="field-label" for="a-last-name">Last Name</label>
+              <input class="field-input" type="text" placeholder="" id="last-name" name="a-last-name">
+            </div>
+          </div>
+
+          <div class="col-sm-8 col-sm-offset-2">
+            <div class="field">
+              <label class="field-label" for="a-email">Email</label>
+              <input class="field-input" type="text" placeholder="" id="email" name="a-email">
+            </div>
           </div>
           <div class="col-sm-8 col-sm-offset-2">
-            <label class="form-control-label" for="last-name">Last Name</label>
-            <input class="form-control" type="text" placeholder="Enter your last name" id="last-name" name="a-last-name">
-          </div>
-        </div>
-        
-        <div class="form-group row">
-          <div class="col-sm-8 col-sm-offset-2">
-            <label class="form-control-label" for="email">Email</label>
-            <input class="form-control" type="text" placeholder="Enter your email" id="email" name="a-email">
-          </div>
-          <div class="col-sm-8 col-sm-offset-2">
-            <label class="form-control-label" for="dob">Subject</label>
-            <textarea class="form-control" placeholder="Subject" id="describe-text" name="a-describe-text" rows="4"></textarea>
+            <br/><br/>
+            <textarea class="field-input-area" placeholder="Subject" id="describe-text" name="a-describe-text" rows="4"></textarea>
           </div>
         </div>
       </div>
@@ -30,3 +36,18 @@
     </div>
     <br>
 </form>
+
+<script>
+  jQuery(function($){
+    $('.field-input').focus(function(){
+        $(this).parent().addClass('is-focused has-label');
+    })
+
+    $('.field-input').blur(function(){
+        if($(this).val() == ''){
+            $(this).parent().removeClass('has-label');
+        }
+        $(this).parent().removeClass('is-focused');
+    })
+})
+</script>
